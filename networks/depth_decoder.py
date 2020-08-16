47ef23184db6e8e006cba44495adebda5d4ba121
sorushn <sorushn@gmail.com>
2020-08-16 18:21:08 +0200

    Revert "Training works but the model returns constant outputs"
    This reverts commit 64175eee7ece0cac5a3d5099e16128841afa9a89.

--- networks/depth_decoder.py
+++ networks/depth_decoder.py
@@ -60,6 +60,6 @@ class DepthDecoder(nn.Module):
             x = torch.cat(x, 1)
             x = self.convs[("upconv", i, 1)](x)
             if i in self.scales:
-                self.outputs["disp_{}".format(i)] = self.sigmoid(self.convs[("dispconv", i)](x))
+                self.outputs[("disp", i)] = self.sigmoid(self.convs[("dispconv", i)](x))
 
         return self.outputs
