1c9793a04b38c98dfe8461e938297069c6440560
focal <bashish101@gmail.com>
2020-10-24 23:05:06 +0200

    Add zero dataset split generation

--- gen_split.py
+++ gen_split.py
@@ -20,8 +20,8 @@ parser = argparse.ArgumentParser(description="Split options")
 ## Choices
 parser.add_argument("--choices", nargs="+", type=int, 
                     help="selected datasets for custom split file - \
-                        datasets: {1: Cityscapes, 2: Yamaha dataset}", 
-                    default=[1,2])
+                        datasets: {1: Cityscapes, 2: Yamaha dataset, 3: Zero dataset}", 
+                    default=[3])
 
 
 ## Cityscapes dataset
@@ -37,7 +37,6 @@ parser.add_argument("--gen_city_split",
                     help="generate cityscapes dataset split",
                     action="store_true", default = False)
 
-
 ## Yamaha dataset
 parser.add_argument("--yamaha_data_path", type=str,
                     help="path to the Yamaha training data",
@@ -50,6 +49,19 @@ parser.add_argument("--yamaha_split_path", type=str,
 parser.add_argument("--gen_yamaha_split", 
                     help="generate yamaha dataset split",
                     action="store_true", default = False)
+
+## Zero dataset
+parser.add_argument("--zero_data_path", type=str,
+                    help="path to the Zero training data",
+                    default="./zero_data")
+
+parser.add_argument("--zero_split_path", type=str,
+                    help="path to the zero dataset split file",
+                    default=os.path.join(splits_path, "zero_split"))
+
+parser.add_argument("--gen_zero_split", 
+                    help="generate zero dataset split",
+                    action="store_true", default = True)
 
 
 opts = parser.parse_args()
@@ -131,6 +143,40 @@ def gen_yamaha_split_data():
 
     val_file.close()
 
+def gen_zero_split_data():
+    
+    train_path = os.path.join(opts.yamaha_data_path, "train")
+    val_path = os.path.join(opts.yamaha_data_path, "val")
+    
+    ## writing yamaha train files
+    train_file = open(opts.yamaha_split_path + os.sep + "train_files.txt", "w")
+    for (_, _, filenames) in os.walk(train_path):
+        filenames = sorted(filenames)
+        all_lines = []
+        out_line = ""
+        for file in filenames:
+            img_head, frame_name = file.split("_")
+            frame_index = frame_name.split('.')[0]
+            out_line = " ".join([img_head, frame_index, "1"])
+            all_lines.append(out_line)
+        all_lines = all_lines[1:-1]
+    
+    data_size = len(all_lines)
+    train_size = int(data_size * 0.8)
+    train_lines = all_lines[:train_size]
+    val_lines = all_lines[train_size:]
+    
+    train_w_lines = map(lambda x:x+'\n', train_lines)
+    train_file.writelines(train_w_lines)  
+    train_file.close()
+    
+    ## writing yamaha val files
+    val_file = open(opts.yamaha_split_path + os.sep + "val_files.txt", "w")
+    val_w_lines = map(lambda x:x+'\n', val_lines)
+    val_file.writelines(val_w_lines)  
+
+    val_file.close()
+
 
 def refine_city_data(path):
     file = open(path, "r")
@@ -163,6 +209,10 @@ def main():
         if choice == 2:
             if opts.gen_yamaha_split:
                 gen_yamaha_split_data()
+                
+        if choice == 3:
+            if opts.gen_zero_split:
+                gen_zero_split_data()
     
         
 if __name__ == "__main__":
